4421a014e5c90944acce9d6885f7cf9d1a2fc99d
riku <riku.suny@gmail.com>
2021-03-27 11:58:26 +0900

    Setup

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Image, View, Dimensions} from 'react-native';
+import R from 'react-native-gesture-handler';
+
+const App = () => {
+  return <View></View>;
+};
+
+export default App;
