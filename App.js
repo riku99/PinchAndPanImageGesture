7b298ce380b81c41b5b605b41ec7ae30c2c970fc
riku <riku.suny@gmail.com>
2021-03-27 13:03:28 +0900

    Zoom-in and Zoom-out by PinchGestureHandler

--- App.js
+++ App.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import {Image, View, Dimensions} from 'react-native';
-import R from 'react-native-gesture-handler';
+import {Image, View, Dimensions, StyleSheet, Animated} from 'react-native';
+import {PinchGestureHandler} from 'react-native-gesture-handler';
+
+const image = require('./flight.jpg');
+
+const {width, height} = Dimensions.get('window');
 
 const App = () => {
-  return <View></View>;
+  // コンポーネントのアニメーションにはAnimated.Valueから生成したオブジェクトが必要
+  const scale = new Animated.Value(1);
+
+  // panningやscrollingなどのジェスチャーに関するイベントを直接animated values(Animated.valueで生成された値)に結びつけるためのシンタックス
+  // 複雑なイベントオブジェクトを簡単に結びつけることに繋がる
+  // あと、確かこのやり方じゃないとuseNativeDriverが使えなかった気がする
+  // listnerを加えることでコールバックも実装できる
+  const onZoomEvent = Animated.event([{nativeEvent: {scale: scale}}], {
+    useNativeDriver: true,
+    listener: () => console.log('this is listner!'),
+  });
+
+  return (
+    <View style={styles.container}>
+      <PinchGestureHandler onGestureEvent={onZoomEvent}>
+        <Animated.Image
+          source={image}
+          style={[styles.image, {transform: [{scale}]}]}
+          resizeMode="contain"
+        />
+      </PinchGestureHandler>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width,
+    height,
+  },
+});
 
 export default App;
